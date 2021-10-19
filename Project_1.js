9e33d1c1ea90f4b6f525737816ebe3c74f4391b3
AmoorhAlshammary <Amirah.Sair.rae.js@tuwaiq.edu.sa>
2021-10-19 06:16:12 +0300

    pro

--- Project_1.js
+++ Project_1.js
@@ -1,7 +1,49 @@
-class users = [
-    { img: "Hijmmmnal.jpg", descrbtion:" p", 
-    
 
-    const studentArr = [
-     
-        
+
+$(document).ready(function(){
+  $('.Box').click(function(){
+  $('#mainpage').hide()
+})
+$(document).click(function(){
+  $('#mainpage').show()
+})
+
+
+    const mainbage=$()
+    const nameInput = $("#name");
+    const imgInput = $("#img");
+    const descriptionInput = $("#description");
+    
+    
+    const users = [
+        { Name: "mmmnal", img: "mmmnal.jpg", description: "p" },
+        { Name: "ahmad", img: "ahmad.jpg",description : "p" }];
+    
+    function renderArr (array) {
+      for (let i = 0; i < array.length; i++) {
+        const div = $ <ul`>
+        <li> Nam :${array[i].Name} img : ${array[i].img} description :${array[i].description}"></li>
+    </ul>
+ 
+    mainpage.append(div);
+
+
+
+      }
+    }
+    renderArr (users)
+
+const mainpage = $("#mainpage")
+
+
+function hideHandler() {
+
+    mainpage.hide();
+    console.log()
+  }
+  
+  function showHandler() {
+   
+    mainbage.show();
+  }
+  
