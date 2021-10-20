1196bc8074147609388dce7a1c86350e28f53fa2
AmoorhAlshammary <Amirah.Sair.rae.js@tuwaiq.edu.sa>
2021-10-20 16:03:45 +0300

    pro

--- Project_1.js
+++ Project_1.js
@@ -1,50 +1,49 @@
 const  mainpge = $("#mainpge");
-function mainpgeHide(){
-  mainpge.hide();
-}
-function mainpgeShow(){
-  mainpge.show();
-}
-function mainpgeRemove(){
-  mainpge.remove();
-}
-
-    
-
-    const main = $("#main");
     const nameInput = $("#name");
     const imgInput = $("#img");
     const descriptionInput = $("#description");
     
-    
     const users = [
-        { Name: "Faisal", img: "pp.jpg", description: "supervisor" },
-        { Name: "Amirah", img: "aa.jpg",description : "painter" }];
+      { Name: "Faisal", img: "pp.jpg", description: "supervisor" },
+      { Name: "Amirah", img: "aa.jpg",description : "painter" }
+    ];
+    function renderArr (array) {
+      for (let i = 0; i < array.length; i++) {
+        const div= $(`<div class="secondryContainer">
+        <img src="${array[i].img}">
+        <li>Nam: ${array[i].Name}</li>
+           <p> ${array[i].description}></p>
+         <button id="del" onclick="deleteFoodAray (${i})">حذف</button>
+        </div>`);
+        mainpge.append(div);
+      }
     
-        function mainpage(array) {
-          for (let i = 0; i < array.length; i++) {
-            const div= $(`<div class="secondryContainer">
-            <li>Nam: ${array[i].Name}</li>
-               <img src="${array[i].img}">
-               <p> ${array[i].description}></p>
-            </div>`);
-            mainpge.append(div);
-          }
-        }
+    }
+
+    renderArr(users);
+
+    function adduser(){
+      const newuser= {
+        Name: nameInput.val(),
+        img: imgInput.val(),
+        description: descriptionInput.val(),
+      };
+      users.push(newuser);
+      renderArr([newuser]);
+    }
     
-        mainpage(users);
-
-        
-          function adduser(){
-          const newuser= {
-            Name: nameInput.val(),
-            img: imgInput.val(),
-            description: descriptionInput.val(),
-          };
-         users.push(newuser);
-         mainpge.html("")
-         mainpage(users)
-
-        //  mainpage([newuser]);
-        }
-        
+    function mainpgeHide(){
+      mainpge.hide();
+    }
+    function mainpgeShow(){
+      mainpge.show();
+    }
+    function mainpgeRemove(){
+      mainpge.remove();
+    }
+    function deleteFoodAray(indexx){
+      users.splice(indexx,1);
+      mainpge.html("");
+     renderArr(users);
+    }
+   
